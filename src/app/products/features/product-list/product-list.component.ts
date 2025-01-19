935784e8100a684d268899e0b2f897c724bce381
zotel1 <zotelsigel@gmail.com>
2025-01-18 22:14:54 -0300

    add, component product-list

---
+++ src/app/products/features/product-list/product-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styles: ``
+})
+export class ProductListComponent {
+
+}
