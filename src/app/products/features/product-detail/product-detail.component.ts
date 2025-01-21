7d5608a1e77421dcb9e46a592b0e232cfe15a78f
zotel1 <zotelsigel@gmail.com>
2025-01-21 15:51:08 -0300

    update product-detail.component

--- src/app/products/features/product-detail/product-detail.component.ts
+++ src/app/products/features/product-detail/product-detail.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, effect, inject, input } from '@angular/core';
+import { ProductDetailStateService } from '../../data-access/product-detail-state';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail.component.html',
-  styles: ``
+  styles: ``,
+  providers: [ProductDetailStateService],
 })
 export class ProductDetailComponent {
+  productDetailState = inject(ProductDetailStateService).state;
+  cartState = inject(CartStateService).state;
+
+  id = input.required<string>();
+
+  constructor() {
+    effect(() => {
+      this.productDetailState.getById(this.id);
+    });
+  }
+
+  addToCart() {
+    this.cartState.add({
+      product: this.productDetailState.product()!,
+      quantity: 1,
+    });
+  }
 
 }
