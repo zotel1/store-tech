82e6ee170e2fc1700e013ed995e659e918ce6779
zotel1 <zotelsigel@gmail.com>
2025-01-22 23:59:59 -0300

    update cart-item component

--- src/app/cart/ui/cart-item/cart-item.component.ts
+++ src/app/cart/ui/cart-item/cart-item.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { ProductItemCart }  from '../../../shared/interfaces/product.interface';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-item',
-  imports: [],
+  standalone: true,
+  imports: [CurrencyPipe],
   templateUrl: './cart-item.component.html',
-  styles: ``
 })
 export class CartItemComponent {
+  productCartItem = input.required<ProductItemCart>();
 
+  onRemove = output<number>();
+
+  onIncrease = output<ProductItemCart>();
+
+  onDecrease = output<ProductItemCart>();
 }
