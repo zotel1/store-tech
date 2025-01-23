402ee65145e3b87e0b39830f1e41801362c981f0
zotel1 <zotelsigel@gmail.com>
2025-01-22 23:57:33 -0300

    add cart-item-component

---
+++ src/app/cart/ui/cart-item/cart-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item',
+  imports: [],
+  templateUrl: './cart-item.component.html',
+  styles: ``
+})
+export class CartItemComponent {
+
+}
